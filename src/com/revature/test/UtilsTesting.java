b3fc43daa1f190ade4eb96d7fdc19479406148ea
Colby Tang <cobtang@gmail.com>
2022-06-22 10:17:56 -0500

    Changed add and added a constructor

--- src/com/revature/test/UtilsTesting.java
+++ src/com/revature/test/UtilsTesting.java
@@ -12,16 +12,13 @@ import src.com.revature.utils.*;
 public class UtilsTesting {
     @Test
     public void ListInstantiation () {
-        List<Integer> testList = new List<>();
-        testList.add (1);
-        testList.add (2);
-        testList.add (3);
+        List<Integer> testList = new List<Integer>(new Integer[]{1,2,3});
         assertEquals("{1, 2, 3}", testList.toString());
     }
 
     @Test
     public void ListGet () {
-        List<Integer> testList = new List<>();
+        List<Integer> testList = new List<Integer>();
         testList.add (1);
         testList.add (2);
         testList.add (3);
@@ -29,5 +26,20 @@ public class UtilsTesting {
         testList.add (7);
         int value = testList.get(3);
         assertEquals(6, value);
+    }
+
+    @Test
+    public void ListAdd () {
+        List<Integer> testList = new List<Integer>();
+        testList.add (5);
+        testList.add (11);
+        testList.add (3);
+        testList.add (10);
+        testList.add (7);
+        assertEquals(5, testList.get(0));
+        assertEquals(11, testList.get(1));
+        assertEquals(3, testList.get(2));
+        assertEquals(10, testList.get(3));
+        assertEquals(7, testList.get(4));
     }
 }
--- src/com/revature/utils/List.java
+++ src/com/revature/utils/List.java
@@ -1,44 +1,54 @@
 package src.com.revature.utils;
 
 import java.lang.Exception;
+import java.lang.RuntimeException;
 
 public class List <T> {
     Node<T> head;
     Node<T> tail;
 
+    public List () {}
+
+    public List (T[] arr) {
+        for (T item : arr) {
+            add(item);
+        }
+    }
+
     public T get (int index) {
         Node<T> returnNode = head;
         for (int i = 0; i < index; i++) {
             if (returnNode.next == null) {
-                // throw new Exception (String.format("List index out of range at [%i]", i+1));
+                // throw new RunTimeException (String.format("List index out of range at [%i]", i+1));
             }
             returnNode = returnNode.next;
         }
         return returnNode.value;
     }
 
+    // Add new node at the end
     public void add (T value) {
-        Node<T> current = head;
-        if (current == null) {
+        if (head == null) {
             head = new Node<T>(value);
+            tail = head;
             return;
         }
-        while (current.next != null) {
-            current = current.next;
+        else {
+            tail.next = new Node<T>(value);
+            tail = tail.next;
         }
-        current.next = new Node<T>(value);
     }
 
     public void remove (T value) {
         throw new UnsupportedOperationException();
     }
 
-    public T removeAtIndex (int index) throws Exception {
+    public T removeAtIndex (int index) {
         Node<T> returnNode = head;
         Node<T> prevNode = null;
         for (int i = 0; i < index; i++) {
             if (returnNode.next == null) {
-                throw new Exception (String.format("List index out of range at [%i]", i+1));
+                // throw new RunTimeException (String.format("List index out of range at [%i]", i+1));
             }
             prevNode = returnNode;
             returnNode = returnNode.next;
